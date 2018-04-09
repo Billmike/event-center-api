6123f68fcbbac3ce0c50be9b1420a1e9c16dbc4e
Ayelegun Kayode Michael <billmike1994@gmail.com>
2018-04-09 12:14:01 +0100

    Write integration tests for user signup and signin

---
+++ server/tests/seed/userseed.js
@@ -0,0 +1,8 @@
+const dummyUser = {
+  id: 4,
+  username: 'davyjones',
+  email: 'davyjones@gmail.com',
+  password: 'qwertyuiop'
+};
+
+export default dummyUser;
---
+++ server/tests/user.test.js
@@ -0,0 +1,171 @@
+import supertest from 'supertest';
+import { expect } from 'chai';
+import app from '../app';
+import dummyUser from './seed/userseed';
+
+const request = supertest(app);
+const signupAPI = '/api/v1/users/signup';
+const signinAPI = '/api/v1/users/signin';
+
+describe('Integration tests for Authentication', () => {
+  describe('Sign-up endpoint tests', () => {
+    it('should fail to create user if no username is provided', (done) => {
+      const testUser = { ...dummyUser };
+      delete testUser.username;
+      request.post(signupAPI)
+        .set('Connection', 'keep alive')
+        .set('Content-Type', 'application/json')
+        .type('form')
+        .send(testUser)
+        .end((error, response) => {
+          expect(response.body).to.be.an('object');
+          expect(response.body.username).to
+            .equal('Enter a username greater than five characters.');
+          expect(response.status).to.equal(400);
+          done();
+        });
+    });
+    it('should fail to create a user if no email is provided', (done) => {
+      const testUser = { ...dummyUser };
+      delete testUser.email;
+      request.post(signupAPI)
+        .set('Connection', 'keep alive')
+        .set('Content-Type', 'application/json')
+        .type('form')
+        .send(testUser)
+        .end((error, response) => {
+          expect(response.body).to.be.an('object');
+          expect(response.body.email).to
+            .equal('Please enter a valid email address.');
+          expect(response.status).to.equal(400);
+          done();
+        });
+    });
+    it(
+      `should fail to create a user if the
+      email supplied is of incorrect format`,
+      (done) => {
+        const testUser = { ...dummyUser };
+        testUser.email = 'qwertyuiop';
+        request.post(signupAPI)
+          .set('Connection', 'keep alive')
+          .set('Content-Type', 'application/json')
+          .type('form')
+          .send(testUser)
+          .end((error, response) => {
+            expect(response.body).to.be.an('object');
+            expect(response.body.email).to
+              .equal('Please enter a valid email address.');
+            expect(response.status).to.equal(400);
+            done();
+          });
+      }
+    );
+    it('should fail to create a user if no password is supplied', (done) => {
+      const testUser = { ...dummyUser };
+      delete testUser.password;
+      request.post(signupAPI)
+        .set('Connection', 'keep alive')
+        .set('Content-Type', 'application/json')
+        .type('form')
+        .send(testUser)
+        .end((error, response) => {
+          expect(response.body).to.be.an('object');
+          expect(response.body.password).to
+            .equal('Your password should be at least 8 characters long.');
+          expect(response.status).to.equal(400);
+          done();
+        });
+    });
+    it('should fail to create a user if the password length is less than or equal to seven', (done) => {
+      const testUser = { ...dummyUser };
+      testUser.password = 'qwert';
+      request.post(signupAPI)
+        .set('Connection', 'keep alive')
+        .set('Content-Type', 'application/json')
+        .type('form')
+        .send(testUser)
+        .end((error, response) => {
+          expect(response.status).to.equal(400);
+          expect(response.body).to.be.an('object');
+          expect(response.body.password).to
+            .equal('Your password should be at least 8 characters long.');
+          done();
+        });
+    });
+    it(
+      'should successfully create a user when all relevant data is supplied',
+      (done) => {
+        request.post(signupAPI)
+          .set('Connection', 'keep alive')
+          .set('Content-Type', 'application/json')
+          .type('form')
+          .send(dummyUser)
+          .end((error, response) => {
+            expect(response.status).to.equal(201);
+            done();
+          });
+      }
+    );
+  });
+  describe('Sign-in endpoint tests', () => {
+    it(
+      'should fail if a user tries to sign-in without providing a email',
+      (done) => {
+        const testUser = {
+          email: dummyUser.email,
+          password: dummyUser.password
+        };
+        delete testUser.email;
+        request.post(signinAPI)
+          .set('Connection', 'keeo alive')
+          .set('Content-Type', 'application/json')
+          .type('form')
+          .send(testUser)
+          .end((error, response) => {
+            expect(response.status).to.equal(400);
+            expect(response.body).to.be.an('object');
+            expect(response.body.email).to
+              .equal('Enter a valid email to sign-in.');
+            done();
+          });
+      }
+    );
+    it(
+      'should fail if a user tries to sign-in without providing a password',
+      (done) => {
+        const testUser = {
+          email: dummyUser.email,
+          password: dummyUser.password
+        };
+        delete testUser.password;
+        request.post(signinAPI)
+          .set('Connection', 'keep alive')
+          .set('Content-Type', 'application/json')
+          .type('form')
+          .send(testUser)
+          .end((error, response) => {
+            expect(response.status).to.equal(400);
+            done();
+          });
+      }
+    );
+    it('should sign-in a user that is already signed up', (done) => {
+      const testUser = { email: dummyUser.email, password: dummyUser.password };
+      request.post(signinAPI)
+        .set('Connection', 'keep alive')
+        .set('Content-Type', 'application/json')
+        .type('form')
+        .send(testUser)
+        .end((error, response) => {
+          expect(response.status).to.equal(201);
+          expect(response.body).to.be.an('object');
+          expect(response.body.message).to.equal('Signin successful.');
+          expect(response.body.email).to.equal('davyjones@gmail.com');
+          expect(response.body.username).to.equal('davyjones');
+          expect(response.body.token).to.be.a('string');
+          done();
+        });
+    });
+  });
+});
